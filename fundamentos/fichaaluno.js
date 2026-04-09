52cb4a6097aafbed7b018751c1d9ee79d4d393e2
MatheusJade Esseyah <matheus.jesseyah@senacsp.edu.br>
2026-04-08 22:14:22 -0300

    ficha aluno update

--- fundamentos/fichaaluno.js
+++ fundamentos/fichaaluno.js
@@ -3,16 +3,20 @@
  * @author Matheus Jade Esseyah
  */
 
+console.log("")
 console.log("-------------------------------")
 console.log("-        ficha do aluno       -")
 console.log("-------------------------------")
+console.log("")
 
 let nome = "Matheus Jade Esseyah"
 let idade = 27
 let peso = 64
-let altura = 160
+let altura = 1.60
+let vip = true
 
 console.log(`Nome: ${nome}`)
 console.log(`Idade: ${idade}`)
-console.log(`Peso: ${peso} kg`)
-console.log(`Altura: ${altura} cm`)
+console.log(`Peso: ${peso}kg`)
+console.log(`Altura: ${altura}m`)
+console.log(`VIP: ${vip}`)
