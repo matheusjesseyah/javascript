a9e03ebe4217b8cad851122df9f5eb056ec4a19e
MatheusJade Esseyah <matheus.jesseyah@senacsp.edu.br>
2026-04-09 20:25:34 -0300

    porcentagem

---
+++ fundamentos/porcentagem.js
@@ -0,0 +1,15 @@
+/**
+ * porcentagem
+ * @author Matheus Jade Esseyah
+ */
+
+
+let valor, porcentagem, total;
+
+valor = 200;
+porcentagem = 10;
+
+total = (valor * porcentagem)/100;
+
+console.log(`${porcentagem}% do valor ${valor} = ${total}`)
+
